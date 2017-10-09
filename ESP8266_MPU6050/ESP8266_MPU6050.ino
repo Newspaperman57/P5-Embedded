b86c4f386c9285829fa8a24e7429982e709462cf
Mathias Pihl <mathias@pihlerne.dk>
2017-10-09 14:05:05 +0200

    Changes hardware-software to work on UDP and other optimizations

--- ESP8266_MPU6050/ESP8266_MPU6050.ino
+++ ESP8266_MPU6050/ESP8266_MPU6050.ino
@@ -2,20 +2,23 @@
 // #include "MPU6050.h"
 #include <Wire.h>
 
+#include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 
 #define MPU_ADDR 0x68
 
-ESP8266WiFiMulti WiFiMulti;
-
+WiFiUDP client;
 const char* ssid = "Tardis";
 const char* password = "geronimo";
 bool state = true;
 
 int b = 100; // Number of datapoints to read before sending. Change size of array on next line accordingly
 int16_t data[100*7];
-char str[64];
+int sent;
+int16_t dataindex;
+int mil;
+int lastMillis;
+IPAddress ipaddr;
 
 void setup_mpu6050() {
   Wire.begin(5,4);
@@ -54,60 +57,41 @@ void setup() {
     // initialize serial communication for debugging purposes
     Serial.begin(115200);
 
-    // initialize device
-    Serial.println("Initializing I2C devices...");
-    
     // Connect to wifi network
-    WiFiMulti.addAP(ssid, password);
-    Serial.print("\nConnecting to wifi");
+    WiFi.begin(ssid, password);
     
+    Serial.print("\nConnecting to wifi");
     // Wait for connection
-    while(WiFiMulti.run() != WL_CONNECTED) {
+    while(WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
         delay(500);
     }
     
     Serial.print("\n");
 
-    // Print the IP address
+    // Print the IP address to serial
     Serial.println(WiFi.localIP());
+
+    ipaddr = IPAddress(192, 168, 43, 166);
+
+    lastMillis = millis();
+    dataindex = 0;
+    sent = 0;
+    //Connect(); // No need to connect when using UDP
+    client.beginPacket(ipaddr, 8085);
 }
 
 void loop() {
-
-    // Define target to send data to
-    const uint16_t port = 8085;
-    const char * host = "192.168.43.155";
-
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections to target
-    WiFiClient client;
-
-    // Wait for connection
-    if (!client.connect(host, port)) {
-        Serial.println("connection failed");
-        Serial.println("wait 1 sec...");
-        delay(1000);
-        return;
-    }
-
-    int16_t index;
-    int mil;
-    int lastMillis = millis();
-    while(1) { // run forever
-        index = 0;
-        // Gather b datapoints
-        for(int i = 0; i < b; i++){  
-            mil = millis();
-            data[index++] = mil -lastMillis;
-            lastMillis = mil;
-            mpu6050_get_data(&data[index++], &data[index++], &data[index++], &data[index++], &data[index++], &data[index++]);
-            // accelgyro.getMotion6(&data[index++], &data[index++], &data[index++], &data[index++], &data[index++], &data[index++]);
-        }
-        // If client doesn't respond to data, stop sending since the connection broke
-        if(client.write((uint8_t*)data, sizeof(int16_t)*b*7) == 0)
-            break;
-    }
+  if(sent > b) { // 4*32 > 7*16
+    client.endPacket();
+    client.beginPacket(ipaddr, 8085);
+    sent = 0;
+  }
+  //Serial.print("Sending data\n");
+  mil = millis();
+  sent++;
+  data[0] = mil -lastMillis;
+  lastMillis = mil;
+  mpu6050_get_data(&data[1], &data[2], &data[3], &data[4], &data[5], &data[6]);
+  client.write((uint8_t*)data, sizeof(int16_t)*7);
 }
