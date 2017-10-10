659795f58c06fe900b109ffc4fe02cacbf375683
Mathias Pihl <mathias@pihlerne.dk>
2017-10-10 14:12:48 +0200

    Some changes

--- ESP8266_MPU6050/CatchDataFromESP8266.py
+++ ESP8266_MPU6050/CatchDataFromESP8266.py
@@ -13,15 +13,11 @@ try:
     except socket.error as msg:
         print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
         sys.exit()
-         
-    print 'Socket bind complete'
-     
-    #Start listening on socket
-#    s.listen(10)
-    print 'Socket now listening'
 
-#    conn, addr = s.accept()
-#    print "Connected to {}".format(addr)
+    print "Bind success, listening on port {}".format(PORT)
+
+
+    s.sendto("GO", ('192.168.43.252', 8085))
 
     file = open(str(sys.argv[1])+".csv", "w")
     file.write("time;x;y;z;rx;ry;rz\n")
@@ -29,23 +25,33 @@ try:
     sys.stdout.write("Receiving data\n")
     totalTime = 0
     while 1:
-        rawdata = s.recvfrom(7*16)
+        rawdata = s.recvfrom(13)
         while rawdata == "":
             #conn, addr = s.accept()
             print "reconnected to {}".format(addr)
             #rawdata = conn.recv(100*7*16)
         data2 = []
-        for i in range(0, len(rawdata[0])/2):
-            num = ((ord(rawdata[0][(i*2)+1])*2**8)+ord(rawdata[0][(i*2)]))
-            if num > 2**15:
-                data2.append(num-2**16)
-            else:
-                data2.append(num)
+        data2.append(ord(rawdata[0][0]))
+
+        data2.append(ord(rawdata[0][1])<<8|ord(rawdata[0][2]))
+        data2.append(ord(rawdata[0][3])<<8|ord(rawdata[0][4]))
+        data2.append(ord(rawdata[0][5])<<8|ord(rawdata[0][6]))
+
+        data2.append(ord(rawdata[0][7])<<8|ord(rawdata[0][8]))
+        data2.append(ord(rawdata[0][9])<<8|ord(rawdata[0][10]))
+        data2.append(ord(rawdata[0][11])<<8|ord(rawdata[0][12]))
+        
+        for i in range(1,len(data2)):
+            if(data2[i] & 1<<15):
+                data2[i] -= 1<<16
+
+        print data2
+
         for i in range(0, len(data2)/7):
             j = i*7
             totalTime += data2[j]
             file.write("{};{};{};{};{};{};{}\n".format(totalTime, data2[j+(0+1)], data2[j+(1+1)], data2[j+(2+1)], data2[j+(3+1)], data2[j+(4+1)], data2[j+(5+1)]))
-        sys.stdout.write('.')
+        #sys.stdout.write('.')
         sys.stdout.flush()
 
 finally:
--- ESP8266_MPU6050/ESP8266_MPU6050.ino
+++ ESP8266_MPU6050/ESP8266_MPU6050.ino
@@ -12,7 +12,7 @@ const char* ssid = "Tardis";
 const char* password = "geronimo";
 bool state = true;
 
-int b = 100; // Number of datapoints to read before sending. Change size of array on next line accordingly
+int b = 1; // Number of datapoints to read before sending. Change size of array on next line accordingly
 int16_t data[100*7];
 int sent;
 int16_t dataindex;
@@ -49,18 +49,20 @@ void mpu6050_get_data(
   *ry=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   *rz=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 }
+int lastread;
 
 void setup() {
-    // join I2C bus
-    setup_mpu6050();
-
     // initialize serial communication for debugging purposes
     Serial.begin(115200);
+
+    Serial.print("Joining I2C\n");
+    // join I2C bus
+    setup_mpu6050();
 
     // Connect to wifi network
     WiFi.begin(ssid, password);
     
-    Serial.print("\nConnecting to wifi");
+    Serial.print("Connecting to wifi");
     // Wait for connection
     while(WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
@@ -72,26 +74,30 @@ void setup() {
     // Print the IP address to serial
     Serial.println(WiFi.localIP());
 
-    ipaddr = IPAddress(192, 168, 43, 166);
-
     lastMillis = millis();
     dataindex = 0;
     sent = 0;
     //Connect(); // No need to connect when using UDP
-    client.beginPacket(ipaddr, 8085);
+    client.begin(8085);
+    client.beginPacket(client.remoteIP(), 8085);
+    lastread = 0;
 }
 
 void loop() {
-  if(sent > b) { // 4*32 > 7*16
-    client.endPacket();
-    client.beginPacket(ipaddr, 8085);
-    sent = 0;
-  }
-  //Serial.print("Sending data\n");
+  client.beginPacket(client.remoteIP(), 8085);
   mil = millis();
-  sent++;
-  data[0] = mil -lastMillis;
+  client.write((int8_t)(mil - lastMillis));
   lastMillis = mil;
-  mpu6050_get_data(&data[1], &data[2], &data[3], &data[4], &data[5], &data[6]);
-  client.write((uint8_t*)data, sizeof(int16_t)*7);
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom((uint8_t)MPU_ADDR,(size_t)14,true);  // request a total of 14 registers
+  client.write(Wire.read()); client.write(Wire.read());  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)   
+  client.write(Wire.read()); client.write(Wire.read());  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)   
+  client.write(Wire.read()); client.write(Wire.read());  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)   
+  Wire.read(); Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  client.write(Wire.read()); client.write(Wire.read());  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)   
+  client.write(Wire.read()); client.write(Wire.read());  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)   
+  client.write(Wire.read()); client.write(Wire.read());  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)   
+  client.endPacket();
 }
