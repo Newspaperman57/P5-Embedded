38611156f0a76c1171257c8756f42c8cbb0ca00e
Christian Grünberg <cmgrunberg@gmail.com>
2017-10-18 14:05:12 +0200

    Add new folder DataHub.Client which will act as the general class for different model types

---
+++ DataHub/DataHub.Client/DataHub.Client/DataHub.Client/DataHubClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHub.Client
+{
+    class DataHubClient
+    {
+        public string ModelTypeID { get; set; }
+
+        public DataHubClient(string modelTypeID)
+        {
+            ModelTypeID = modelTypeID;
+        }
+
+        public TestInfo GetTest()
+        {
+
+        }
+
+        //method for sending data
+        public 
+    }
+}
---
+++ DataHub/DataHub.Client/DataHub.Client/DataHub.Client/Messages/TestInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHub.Client.Messages
+{
+    class TestInfo
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public int ModelTypeId { get; set; }
+        public Label[] Labels { get; set; }
+        public Parameter[] Parameters { get; set; }
+        public DataLabelSet[] TrainingSet { get; set; }
+        public TestDataSet[] TestSet { get; set; }
+    }
+}
